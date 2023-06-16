487bdd2b4124d9b13b7cd13751cc0dc44d46c4d1
Roberto Cestari <robertotcestari@gmail.com>
2023-06-16 09:43:31 -0300

    update badge and add post

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,14 +2,14 @@ export function Badge({
   className,
   compact = false,
   large = false,
-  type = 'Código',
+  type = 'Produto',
   ...props
 }) {
   return (
     <div>
       <span
         className={`${
-          type === 'Código'
+          type === 'Produto'
             ? 'bg-blue-200 text-blue-800'
             : 'bg-purple-300 text-purple-800'
         }  rounded-full p-1 px-2 text-xs ${className}`}
